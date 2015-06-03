8aa63cec1fd60d3013ba544733b3e1a9b8ab88a4
sthordall <stephantl@gmail.com>
2015-06-03 15:25:32 +0200

    Continued work on Calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+   
+    var userTyping = false
+    var operandStack = Array<Double>()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func enter(sender: UIButton) {
+        
     }
-
+    
+    
+    
 
 }
 
