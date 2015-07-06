34955ee922404b2cc5a5f6e340b5185a0e0c4c98
sthordall <stephantl@gmail.com>
2015-07-06 17:32:40 +0200

    Autolayout hell done

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Stephan Thordal Larsen on 03/07/15.
+//  Copyright (c) 2015 Stephan Thordal. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+   
+}
